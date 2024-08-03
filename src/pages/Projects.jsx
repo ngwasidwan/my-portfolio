26f6d233247ab9c7a5394861c2fdcc7d680563e6
Ngwa Sidwan <131943139+ngwasidwan@users.noreply.github.com>
2024-08-03 06:11:43 +0100

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,40 +1,16 @@
-import { BiPlay } from "react-icons/bi";
+
+import Images from "../components/Images";
 
 function projects() {
   return (
-    <div className="grid md:grid-cols-2 md:gap-10 gap-20 flex-col md:flex-row  my-12 grid-cols-1  pb-5 text-stone-100">
-      <div className="z-10">
-        <img src="img-4.png" alt="projects" />
-
-        <h1 className="text-lg mt-4 font-semibold mb-4">
-          Queen Mother&apos;s Cuisine
-        </h1>
-        <button className="flex items-center gap-2  px-2 py-1 rounded-md text-sm  cursor-pointer bg-none bg-stone-800 hover:bg-stone-900 border hover:border-stone-100 border-stone-600 transition-all">
-          <span>Demo</span>
-          <BiPlay />
-        </button>
-      </div>
-      <figure>
-        <img src="img-1.png" alt="projects" />
-
-        <figcaption className="text-lg mt-4 font-semibold">
-          City Tours
-        </figcaption>
-      </figure>
-      <figure>
-        <img src="img-2.png" alt="projects" />
-
-        <figcaption className="text-lg mt-4 font-semibold">
-          Entertainment App
-        </figcaption>
-      </figure>
-      <figure>
-        <img src="img-3.png" alt="projects" />
-
-        <figcaption className="text-lg mt-4 font-semibold">
-          Frontend Quiz
-        </figcaption>
-      </figure>
+    <div className="grid md:grid-cols-2 md:gap-10 gap-20 flex-col md:flex-row  my-12 grid-cols-1  pb-5 text-stone-100 relative">
+      <Images
+        src="image-1.png"
+        title="Easy Travels"
+        link="easy-travels.vercel.app"
+      />
+      <Images src="image-2.png" title="Queen Mother's Cuisine" />
+      <Images src="image-3.png" title="Entertainment App" />
     </div>
   );
 }
